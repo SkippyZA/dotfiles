a8a0709783fb69b9fc27264018a953c3062f2884
Steven Inskip <inskips@gmail.com>
2024-08-29 17:17:07 +0200

    extra go config and updated lock file

--- files/nvim/.config/lua/lua/plugins/vim-go.lua
+++ files/nvim/.config/lua/lua/plugins/vim-go.lua
@@ -1,3 +1,15 @@
+local generalSettingsGroup = vim.api.nvim_create_augroup('General settings', { clear = true })
+
+function build_go_files()
+  local file = vim.fn.expand('%')
+  if string.match(file, "_test.go$") then
+    vim.cmd([[ GoTestFile ]])
+  elseif string.match(file, ".go$") then
+    vim.cmd([[ GoBuild ]])
+  end
+end
+
+
 return {
   "fatih/vim-go",
   ft = "go",
@@ -5,6 +17,13 @@ return {
     vim.cmd.GoInstallBinaries()
   end,
   config = function()
-    -- do something
+    vim.keymap.set('n', '<leader>a', [[ <cmd>GoAlternate<cr> ]], { desc = "Switch between source and test files" })
+    vim.keymap.set('n', '<leader>b', [[ :lua build_go_files()<cr> ]], { desc = "Build or test golang file based on filename" })
+
+    vim.g.go_def_mode='gopls'
+    vim.g.go_info_mode='gopls'
+
+    vim.g.go_def_mapping_enabled = 0   -- disable vim-go :GoDef short cut (gd), this is instead handled by LanguageClient [LC]
+    vim.g.go_fmt_command = "goimports" -- use goimports instead of gofmt when saving
   end
 }
